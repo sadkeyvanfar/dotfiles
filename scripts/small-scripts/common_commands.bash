327b35eda4f3a2cfd48285b74eddb2692e0fe56a
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-26 03:41:01 +0100

    add dd common usages

--- scripts/small-scripts/common_commands.bash
+++ scripts/small-scripts/common_commands.bash
@@ -20,3 +20,12 @@ tar -cfz test.tar.gz .
 
 # unarchive and unzip with gunzip
 tar -xf test.tar.gz
+
+# write to dick
+dd if=test.iso of=/dev/sdb bs=4096
+
+# backup
+dd if=/dev/sda1 of=sda1-backup.dd bs=4096
+
+# create big files
+dd if=/dev/zero of=lg.test bs=100M count=1
