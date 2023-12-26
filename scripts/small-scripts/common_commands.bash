88532cb8e60aa244fe4f28c6520ee59ed7b5868e
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-26 12:53:26 +0100

    update common_commands.bash

--- scripts/small-scripts/common_commands.bash
+++ scripts/small-scripts/common_commands.bash
@@ -29,3 +29,15 @@ dd if=/dev/sda1 of=sda1-backup.dd bs=4096
 
 # create big files
 dd if=/dev/zero of=lg.test bs=100M count=1
+
+# separate output form errors
+ls v* > output 2> error
+
+# write to modem
+echo "00101010" > /dev/modem
+
+# show just errors
+ls -R / 1> /dev/null
+
+# run script on background and send both normal output and errors to the same file
+nohup script.sh > output 2>&1 &
