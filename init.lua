3430cebaeca8db0124908421899391d29c1e01fc
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-05-11 19:31:11 +0200

    add alpha plugin

--- init.lua
+++ init.lua
@@ -27,4 +27,5 @@ require('lazy').setup({
   require 'plugins.autocompletion',
   require 'plugins.none-ls',
   require 'plugins.gitsigns',
+  require 'plugins.alpha',
 })
---
+++ plugins/alpha.lua
@@ -0,0 +1,26 @@
+-- [[ plugin: alpha-nvim ]]
+-- This plugin provides a start screen for Neovim, similar to the one in Vim.
+return {
+  'goolord/alpha-nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.startify'
+
+    dashboard.section.header.val = {
+      [[                                                    ]],
+      [[ ████████╗        ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+      [[ ╚═╗██╔══╝        ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+      [[   ║██║   ██████╗ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+      [[   ║██║   ╚═════╝ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+      [[   ║██║           ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+      [[   ╚══╝           ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+      [[                                                    ]],
+    }
+
+    alpha.setup(dashboard.opts)
+  end,
+}
